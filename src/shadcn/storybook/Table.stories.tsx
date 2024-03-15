740723f899c9f5fa047ce778d1daf15bc2da8c3f
Aidan Glenister <aidan.glenister@moesol.com>
2024-03-15 16:12:55 -0700

    initial shadcn component adds to conveyor 2.0.0

---
+++ src/shadcn/storybook/Table.stories.tsx
@@ -0,0 +1,100 @@
+import { ListGroup } from 'react-bootstrap';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import {
+  Slots,
+  Slot,
+  Navbar,
+  Alert,
+  Conveyor,
+  TableBody,
+  useData,
+  Lenses,
+  Lens,
+  TableRowState,
+  Table,
+} from '@/index';
+import { Button } from '../components/ui/button';
+
+
+const meta = {
+  title: 'shadcn/components/Table',
+  component: Slots,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    slotKeys: ['slot1', 'slot2', 'slot3'],
+    children: (
+      <>
+        <Slot slotKey='slot1'>
+          <ListGroup.Item>It's </ListGroup.Item>
+        </Slot>
+        <Slot slotKey='slot2'>
+          <ListGroup.Item>a </ListGroup.Item>
+        </Slot>
+        <Slot slotKey='slot3'>
+          <ListGroup.Item>Beaver!</ListGroup.Item>
+        </Slot>
+        <Slot slotKey='secret-slot'>
+          <ListGroup.Item>Or is it?!</ListGroup.Item>
+        </Slot>
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: (props) => (
+    <ListGroup>
+      <Slots {...props} />
+    </ListGroup>
+  ),
+} satisfies Meta<typeof Slots>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+const DemoCell = () => {
+  const { data } = useData();
+  return (
+    <>
+      <Lens lens={TableRowState.VALUE}>{data.hello} monkey</Lens>
+      <Lens lens={TableRowState.EDIT}>noinput</Lens>
+    </>
+  );
+};
+
+export const TableTest: Story = {
+  args: {
+    children: (
+      <>
+      <Navbar modelNames={['hello']}/>
+        {/* <Navbar modelNames={['hello']}/> */}
+        <Table
+          fields={['hello', 'world']}
+          data={[
+            { hello: 'hello', world: 'world' },
+            { hello: 'hellow', world: 'wurld' },
+          ]}
+          actionsConfig={{ showActions: true }}
+        >
+          <Table.Body>
+            <Table.Row>
+              <td>
+                <DemoCell />
+              </td>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <Button onClick={(e)=> console.log('clicked')} variant='success' size={'sm'}>{'Button'}</Button>
+      </>
+    ),
+  },
+};
